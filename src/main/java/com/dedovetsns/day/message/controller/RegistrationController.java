ed866a477434aee0b49a767f36cda7318f38ed00
DedovetsNS <kokocik3@bk.ru>
2020-04-11 20:02:02 +0300

    password encoder added

--- src/main/java/com/dedovetsns/day/message/controller/RegistrationController.java
+++ src/main/java/com/dedovetsns/day/message/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.dedovetsns.day.message.model.Role;
 import com.dedovetsns.day.message.model.User;
 import com.dedovetsns.day.message.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +16,12 @@ import java.util.Map;
 public class RegistrationController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationController(UserRepository userRepository) {
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/registration")
@@ -36,6 +39,7 @@ public class RegistrationController {
         }
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "redirect:/login";
     }
